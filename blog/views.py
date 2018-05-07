75a053187c5c375131c2299124249a57167437fe
will <will@it-c-3.local.lan>
2018-05-07 10:19:00 +0800

    sally

--- blog/views.py
+++ blog/views.py
@@ -5,8 +5,8 @@ def home_page(request):
         our_comment = request.POST.get('comment')
         our_user = request.POST.get("username")
         comment_obj = Comment.objects.create(
-            comment=our_comment,
-            user=our_user
+            comment = our_comment,
+            user = our_user
         )
 
         return render(request, 'blog/home_page.html', {'comment': comment_obj,})
